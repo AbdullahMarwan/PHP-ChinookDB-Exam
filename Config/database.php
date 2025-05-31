9bdb8b188e88fb771a04ffd21221f2fb01c4fa1e
AbdullahMarwan <abdu.moussa01@gmail.com>
2025-05-31 21:44:01 +0200

    env check

--- Config/database.php
+++ Config/database.php
@@ -3,8 +3,11 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
-$dotenv->load();
+$envPath = __DIR__ . '/../.env';
+if (file_exists($envPath)) {
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+    $dotenv->load();
+}
 
 class Database {
     private static $instance = null;
